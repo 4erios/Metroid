7969256fd921f025e7e9f112b15844dbe7f66120
Nagashka <e.potdevin@rubika-edu.com>
2019-03-19 18:48:33 +0100

    Systems de la vie et de la mort

---
+++ Retro - Metroid/Assets/Scripts/Naga/DamageSystem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageSystem : MonoBehaviour
+{
+    //Life
+    protected int lifeMax;
+    [SerializeField]
+    protected int currentLife;
+    public int readCurentLife;
+
+    //Damage taken
+    public int lifeFlux;
+
+    protected void LifeSystem()
+    {
+        if (lifeFlux != 0)
+        {
+            currentLife -= lifeFlux;
+            lifeFlux = 0;
+        }
+    }
+}
---
+++ Retro - Metroid/Assets/Scripts/Naga/PlayerLifeSystem.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLifeSystem : DamageSystem
+{
+    //Stack
+    private int maxStack = 0;
+    [SerializeField]
+    private int stack = 0;
+    [HideInInspector]
+    public int readPercentStack;
+
+    private void Start()
+    {
+        lifeMax = 99;
+        currentLife = 30;
+    }
+
+    private void Update()
+    {
+        readCurentLife = currentLife;
+
+        ForUIStack();
+
+        StackSystem();
+
+        LifeSystem();
+
+        DeadSystem();
+    }
+
+
+    private void ForUIStack()
+    {
+        readPercentStack = stack / 10;
+    }
+
+    private void StackSystem()
+    {
+        if (lifeMax < currentLife)
+        {
+            if (stack < maxStack)
+                stack++;
+            currentLife = lifeMax;
+        }
+
+    }
+
+    private void DeadSystem()
+    {
+        if (currentLife <= 0)
+        {
+            stack--;
+            currentLife = 99 + currentLife;
+        }
+
+        if (stack < 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
