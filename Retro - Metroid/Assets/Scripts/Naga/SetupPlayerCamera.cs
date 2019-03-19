a45a809f5cbdd5e5835bdaa589959a61e174b2d5
Nagashka <e.potdevin@rubika-edu.com>
2019-03-19 23:02:41 +0100

    Système de caméra

---
+++ Retro - Metroid/Assets/Scripts/Naga/SetupPlayerCamera.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class SetupPlayerCamera : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject Camera;
+
+    private void Update()
+    {
+        if(this.gameObject.GetComponent<CinemachineVirtualCamera>().Follow == null)
+            this.gameObject.GetComponent<CinemachineVirtualCamera>().Follow = Camera.GetComponent<CameraScript>().player;
+    }
+}
