f1d01477911eb502e50cf2a4a3839a66b97e5de9
justanotheridentity <justanotheridentity12@gmail.com>
2019-03-21 12:29:06 +0100

    Basic jump and Move

--- Retro - Metroid/Assets/Scripts/Controller Mouvement/MoveJump.cs
+++ Retro - Metroid/Assets/Scripts/Controller Mouvement/MoveJump.cs
@@ -2,17 +2,85 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
+
 public class MoveJump : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //Variables
+    private Rigidbody rb;
+    private float axeHorizontal;
+    /// private float axeVertical;
+    /// 
+
+    public float moveSpeed = 40;
+    public float jumpForce = 150;
+    public float gravity = -250f;
+
+    [SerializeField] private bool isGrounded;
+    [SerializeField] private LayerMask layerGround;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float groundRadius = .01f;
+
+    private void Awake()
+    {
+        Debug.Log(Physics.gravity);
+    }
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.useGravity = false;
+    }
+
+    private void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
+        Move();
+        Jump();
+
+        CheckThatGround();
+        ThereGoesGravity();
+    }
+
+    void Move ()
+    {
+        axeHorizontal = Input.GetAxis("Horizontal") ;
+        rb.velocity = new Vector3(axeHorizontal,0,0);
+    }
+
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Joystick1Button0)&& isGrounded)
+        {
+            rb.AddForce(0, jumpForce, 0, ForceMode.Impulse);
+        }
+    }
+
+    void CheckThatGround ()
+    {
+        isGrounded = false;
         
+        Collider[] colliders = Physics.OverlapSphere(groundCheck.position, groundRadius, layerGround);
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            if (colliders[i].gameObject != gameObject)
+            {
+                isGrounded = true;
+                Debug.Log("player is on ground");
+            }
+        }
+    }
+
+    void ThereGoesGravity()
+    {
+        if (isGrounded == false)
+        {
+            //rb.AddForce(Physics.gravity, ForceMode.Acceleration);
+            rb.AddForce(0, gravity, 0, ForceMode.Acceleration);
+        }
     }
 }
