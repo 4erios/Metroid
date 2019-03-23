167939005a291c3230ffd049e90f78915c22cf96
Unknown <justanotheridentity12@gmail.com>
2019-03-24 00:41:15 +0100

    amélioration mais encore foireux

--- Retro - Metroid/Assets/Scripts/Controller Mouvement/MoveJump.cs
+++ Retro - Metroid/Assets/Scripts/Controller Mouvement/MoveJump.cs
@@ -14,12 +14,21 @@ public class MoveJump : MonoBehaviour
 
     public float moveSpeed = 40;
     public float jumpForce = 150;
-    public float gravity = -250f;
+    public float gravity = 250f;
 
     [SerializeField] private bool isGrounded;
     [SerializeField] private LayerMask layerGround;
     [SerializeField] private Transform groundCheck;
+    [SerializeField] private Transform ceilCheck;
     [SerializeField] private float groundRadius = .01f;
+
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier =  2f;
+
+    [SerializeField] private bool inverseGravity = false;
+
+    KeyCode JumpButton = KeyCode.Joystick1Button0;
+    KeyCode GravityButton = KeyCode.Joystick1Button1;
 
     private void Awake()
     {
@@ -29,7 +38,7 @@ public class MoveJump : MonoBehaviour
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
-        rb.useGravity = false;
+        //rb.useGravity = false;
     }
 
     private void Update()
@@ -39,47 +48,108 @@ public class MoveJump : MonoBehaviour
 
     private void FixedUpdate()
     {
+        CheckThatGround();
+        //ThereGoesGravity();
+
         Move();
         Jump();
+        GravityChange();
 
-        CheckThatGround();
-        ThereGoesGravity();
+        
     }
 
     void Move ()
     {
         axeHorizontal = Input.GetAxis("Horizontal") ;
-        rb.velocity = new Vector3(axeHorizontal,0,0);
+        rb.velocity = new Vector3(axeHorizontal * moveSpeed,rb.velocity.y,rb.velocity.z);
     }
 
     void Jump()
     {
-        if (Input.GetKeyDown(KeyCode.Joystick1Button0)&& isGrounded)
+        if (inverseGravity == false)
         {
-            rb.AddForce(0, jumpForce, 0, ForceMode.Impulse);
+            if (Input.GetKeyDown(JumpButton) && isGrounded && inverseGravity == false)
+            {
+                //rb.AddForce(0, jumpForce, 0, ForceMode.Impulse);
+                rb.velocity = jumpForce * Vector3.up;
+            }
+
+            Debug.Log("GO");
+
+            Debug.Log("velocity " + rb.velocity);
+            Debug.Log("velocitYY " + rb.velocity.y);
+
+            if (rb.velocity.y < 0)
+            {
+                rb.velocity += Vector3.up * Physics.gravity.y * (fallMultiplier - 1) * Time.fixedDeltaTime;
+                Debug.Log("TEST000");
+            }
+            else if (rb.velocity.y > 0 && Input.GetKey(JumpButton))
+            {
+                rb.velocity += Vector3.up * Physics.gravity.y * (lowJumpMultiplier - 1) * Time.fixedDeltaTime;
+                Debug.Log("test2");
+            }
+
+            Debug.Log("velocity AFTER" + rb.velocity);
+        }
+        else
+        { 
+            if (Input.GetKeyDown(JumpButton) && isGrounded && inverseGravity == true)
+            {
+                rb.AddForce(0, -jumpForce, 0, ForceMode.Impulse);
+            }
         }
     }
 
-    void CheckThatGround ()
+    void GravityChange()
+    {
+        if (Input.GetKeyDown(GravityButton))
+        {
+            inverseGravity = !inverseGravity;
+        }
+    }
+
+    void CheckThatGround()
     {
         isGrounded = false;
-        
-        Collider[] colliders = Physics.OverlapSphere(groundCheck.position, groundRadius, layerGround);
-        for (int i = 0; i < colliders.Length; i++)
+
+        if (!inverseGravity)
         {
-            if (colliders[i].gameObject != gameObject)
+            Collider[] colliders = Physics.OverlapSphere(groundCheck.position, groundRadius, layerGround);
+            for (int i = 0; i < colliders.Length; i++)
             {
-                isGrounded = true;
-                Debug.Log("player is on ground");
+                if (colliders[i].gameObject != gameObject)
+                {
+                    isGrounded = true;
+                    Debug.Log("player is on ground");
+                }
+            }
+        }
+        else
+        {
+            Collider[] colliders = Physics.OverlapSphere(ceilCheck.position, groundRadius, layerGround);
+            for (int i = 0; i < colliders.Length; i++)
+            {
+                if (colliders[i].gameObject != gameObject)
+                {
+                    isGrounded = true;
+                    Debug.Log("player is on ceil");
+                }
             }
         }
     }
 
     void ThereGoesGravity()
     {
-        if (isGrounded == false)
+        if (isGrounded == false && inverseGravity ==false)
         {
             //rb.AddForce(Physics.gravity, ForceMode.Acceleration);
+            //rb.AddForce(0, -gravity, 0, ForceMode.Acceleration);
+            rb.velocity = new Vector3 (rb.velocity.x, -gravity, rb.velocity.z);
+            Debug.Log("b****" + rb.velocity);
+        }
+        else if(isGrounded == false && inverseGravity == true)
+        {
             rb.AddForce(0, gravity, 0, ForceMode.Acceleration);
         }
     }
