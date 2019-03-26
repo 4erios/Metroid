87913921cfef159b2a70e8b3b55cc5e543aba2e3
Adrien <a.dourgnon@rubika-edu.com>
2019-03-26 10:07:11 +0100

    Création de folders et de script Adrien

---
+++ Retro - Metroid/Assets/Scripts/Adrien_Script/Player_sphere.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_sphere : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float sphereSize;
+
+    void Start()
+    {
+        sphereSize = GetComponent<GameObject>(BoxCollider2D.size.x);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey("Down"))
+        {
+          
+
+        }
+    }
+}
