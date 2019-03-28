6abc0b3d7d882eb49f6eebadaf4bc546a8578291
Nagashka <e.potdevin@rubika-edu.com>
2019-03-28 16:59:32 +0100

    Pas mal d'UI dont celle de la vie!

---
+++ Retro - Metroid/Assets/Scripts/Naga/UI/UIPlayerScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIPlayerScript : MonoBehaviour
+{
+    public GameObject energyText;
+    private int curentLife;
+    // public GameObject missileText;
+    // private int currentMissile;
+    // public GameObject stackText;
+
+    void Update()
+    {
+        curentLife = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerLifeSystem>().readCurentLife;
+        energyText.GetComponent<Text>().text = curentLife.ToString();
+        // missileText.GetComponent<Text>().text = currentMissile.ToString();
+    }
+}
