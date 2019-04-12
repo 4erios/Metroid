d3d504a5f5965ac7b207b5a99d6e559861e31b12
Unknown <adelou001@hotmail.com>
2019-04-12 23:09:14 +0200

    Level 2 Done

--- Retro - Metroid/Assets/Scripts/Naga/Environnement/LavaScript.cs
+++ Retro - Metroid/Assets/Scripts/Naga/Environnement/LavaScript.cs
@@ -9,7 +9,7 @@ public class LavaScript : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.tag == "Player")
+        if (collision.tag == "Player")
         {
             collision.GetComponent<PlayerLifeSystem>().TakeDamage(damage);
         }
